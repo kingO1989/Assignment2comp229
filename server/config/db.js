2305cc9c683058e982caecc0e24218de7c4f389d
Abdulsaheed <kingolaribigbe@gmail.com>
2021-06-19 23:07:37 -0400

    fifth commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports=
 {
-    "URI":"mongodb://localhost/contacts"
+  // "URI":"mongodb://localhost/contacts"
+    "URI":"mongodb+srv://Saheed22:Saheed22@cluster0.lirry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
